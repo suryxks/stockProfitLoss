0cc1f40c11ce40f5cf0cfc031d758cfdf50256b0
surya <suryasuresh9876543@gmail.com>
2021-08-14 20:02:15 +0530

    update: added eventlisteners to input and output elementd

---
+++ script.js
@@ -0,0 +1,6 @@
+var purchasePriceInput=document.querySelector("#purchase-price");
+var stockQuantityInput=document.querySelector("#stock-quantity");
+var currentPriceInput=document.querySelector("#current-price");
+var checkButton=document.querySelector("#check-btn");
+var outputEl=document.querySelector("#message");
+console.log(purchasePriceInput,stockQuantityInput,currentPriceInput,checkButton,outputEl);
