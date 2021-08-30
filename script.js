c648533a9b99cc4af79dd20eede8c485bc0b0462
surya <suryasuresh9876543@gmail.com>
2021-08-30 09:37:28 +0530

    refactor: refactored the app to take only positive values

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-var purchasePriceInput=document.querySelector("#purchase-price");
-var stockQuantityInput=document.querySelector("#stock-quantity");
-var currentPriceInput=document.querySelector("#current-price");
-var checkButton=document.querySelector("#check-btn");
-var outputEl=document.querySelector("#message");
+var purchasePriceInput = document.querySelector("#purchase-price");
+var stockQuantityInput = document.querySelector("#stock-quantity");
+var currentPriceInput = document.querySelector("#current-price");
+var checkButton = document.querySelector("#check-btn");
+var outputEl = document.querySelector("#message");
 
-function lossCalculator(loss,diff,cost){
-  var lossPercentage=(loss/cost)*100;
-  outputEl.style.color="red";
-  outputEl.innerHTML=`Sorry for your loss you have lost ${lossPercentage.toFixed(2)}% which is ${loss}`
+function lossCalculator(loss, diff, cost) {
+  var lossPercentage = (loss / cost) * 100;
+  outputEl.style.color = "red";
+  outputEl.innerHTML = `Sorry for your loss you have lost ${lossPercentage.toFixed(2)}% which is ${loss}`
 }
-function profitCalculator(profit,diff,cost){
-    var profitPercentage=(profit/cost)*100;
-  
-    outputEl.innerHTML=`congratulations you have gained a profit of ${profitPercentage.toFixed(2)}% which is ${profit}`
+
+function profitCalculator(profit, diff, cost) {
+  var profitPercentage = (profit / cost) * 100;
+
+  outputEl.innerHTML = `congratulations you have gained a profit of ${profitPercentage.toFixed(2)}% which is ${profit}`
+}
+
+function clickHandler() {
+  if (purchasePriceInput.value === "" || stockQuantityInput.value === "" || currentPriceInput.value === "") {
+    alert("Please fill all the required fields");
   }
-function clickHandler(){
-    if(purchasePriceInput.value===""||stockQuantityInput.value===""||currentPriceInput.value===""){
-        alert("Please fill all the required fields");
+  var purchasePrice = Number(purchasePriceInput.value);
+  var stockQuantity = Number(stockQuantityInput.value);
+  var currentPrice = Number(currentPriceInput.value);
+  if (purchasePrice < 0 || stockQuantity < 0 || currentPrice < 0) {
+    outputEl.innerHTML = "please Enter positive values"
+  } else {
+    if (purchasePrice > currentPrice) {
+      var priceDiff = purchasePrice - currentPrice;
+      var totalCostOfBuying = purchasePrice * stockQuantity;
+      var loss = purchasePrice * stockQuantity - currentPrice * stockQuantity;
+      lossCalculator(loss.toFixed(2), priceDiff.toFixed(2), totalCostOfBuying.toFixed(2));
+    } else if (purchasePrice === currentPrice) {
+      outputEl.innerHTML = "You have neither gained nor lost anything check again after few days";
+    } else {
+      var priceDiff = currentPrice - purchasePrice;
+      var totalCostOfBuying = purchasePrice * stockQuantity;
+      var profit = currentPrice * stockQuantity - purchasePrice * stockQuantity;
+
+      profitCalculator(profit.toFixed(2), priceDiff.toFixed(2), totalCostOfBuying.toFixed(2));
     }
-    var purchasePrice=Number(purchasePriceInput.value);
-    var stockQuantity=Number(stockQuantityInput.value);
-    var currentPrice=Number(currentPriceInput.value);
-  if(purchasePrice>currentPrice){
-      var priceDiff=purchasePrice-currentPrice;
-      var totalCostOfBuying=purchasePrice*stockQuantity;
-      var loss=purchasePrice*stockQuantity-currentPrice*stockQuantity;
-      lossCalculator(loss.toFixed(2),priceDiff.toFixed(2),totalCostOfBuying.toFixed(2));
-  }else if(purchasePrice===currentPrice){
-    outputEl.innerHTML="You have neither gained nor lost anything check again after few days";
-}else{
-    var priceDiff=currentPrice-purchasePrice;
-    var totalCostOfBuying=purchasePrice*stockQuantity;
-    var profit=currentPrice*stockQuantity-purchasePrice*stockQuantity;
-    console.log(profit,priceDiff,totalCostOfBuying);
-    profitCalculator(profit.toFixed(2),priceDiff.toFixed(2),totalCostOfBuying.toFixed(2));
+
   }
-  
-    
 
 }
-checkButton.addEventListener("click",clickHandler);
+checkButton.addEventListener("click", clickHandler);
